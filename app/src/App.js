a82bfe8afd71139b583ab5af349b82ba75edd969
Sathwik Pochampally <sathwik@outlook.com>
2019-09-13 16:47:49 -0500

    Changed landing page

--- app/src/App.js
+++ app/src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Auth() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 class="text-center">ClickIn</h1>
+        <h2 class="text-center subheader">An online system for problem-driven learning.</h2>
+                <div class="login-methods">
+                    <a class="btn btn-success w-100" href="/pl" role="button">
+                      <span class="font-weight-bold">Developer by-pass</span>
+                    </a>
+                    <br/>
+                    <a class="btn btn-illinois w-100 position-relative" href="/pl/shibcallback" role="button">
+                      <img src="/images/illinois_logo.svg" class="social-icon"/>
+                      <span class="font-weight-bold">Sign in with Illinois</span>
+                    </a>
+                    <br/>
+                    <a class="btn btn-primary w-100 position-relative" href="/pl/oauth2login" role="button">
+                      <img src="/images/google_logo.svg" class="social-icon"/>
+                      <span class="font-weight-bold">Sign in with Google</span>
+                    </a>
+                    <br/>
+                    <a class="btn btn-dark w-100 position-relative" href="/pl/azure_login" role="button">
+                      <img src="/images/ms_logo.svg" class="social-icon"/>
+                      <span class="font-weight-bold">Sign in with Microsoft</span>
+                    </a>
+                    <br/>
+                </div>
       </header>
     </div>
   );
 }
 
-export default App;
+export default Auth;
