483afa125c476c2c6c0e131d29af5a1b57007564
Brian Strauch <bstrauch24@gmail.com>
2019-09-19 08:35:48 -0500

    Basic remote skeleton

--- app/src/components/Auth/index.jsx
+++ app/src/components/Auth/index.jsx
@@ -4,26 +4,15 @@ export default class Auth extends React.Component {
   render() {
     return (
       <div className="Auth">
-        <h1 class="text-center">Kaboom</h1>
-        <h2 class="text-center subheader">Open Source Attendance Tool.</h2>
+        <h1>Kaboom</h1>
+        <h2>Open Source Attendance Tool</h2>
 
-        <div class="login-methods">
-            <a class="btn btn-success w-100" href="/pl" role="button">
-              <span class="font-weight-bold">Developer bypass</span>
-            </a>
-            <br/>
-            <a class="btn btn-illinois w-100 position-relative" href="/pl/shibcallback" role="button">
-              <span class="font-weight-bold">Sign in with Illinois</span>
-            </a>
-            <br/>
-            <a class="btn btn-primary w-100 position-relative" href="/pl/oauth2login" role="button">
-              <span class="font-weight-bold">Sign in with Google</span>
-            </a>
-            <br/>
-            <a class="btn btn-dark w-100 position-relative" href="/pl/azure_login" role="button">
-              <span class="font-weight-bold">Sign in with Microsoft</span>
-            </a>
-          </div>
+        <div>
+          <a href="/remote"><p>Developer Bypass</p></a>
+          <p>Sign in with Illinois</p>
+          <p>Sign in with Google</p>
+          <p>Sign in with Microsoft</p>
+        </div>
       </div>
     );
   }
--- app/src/components/Remote/index.jsx
+++ app/src/components/Remote/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import openSocket from 'socket.io-client';
 
 export default class Remote extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: []
+    };
+  }
+
+  componentDidMount() {
+    const socket = openSocket('http://localhost:5000');
+  }
+
   render() {
     return (
       <div className="Remote">
-        <button>A</button>
-        <button>B</button>
-        <button>C</button>
-        <button>D</button>
-        <button>E</button>
+        <div>
+          <button>A</button>
+          <button>B</button>
+          <button>C</button>
+          <button>D</button>
+          <button>E</button>
+        </div>
       </div>
     );
   }
