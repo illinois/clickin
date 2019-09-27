e8f77c918506c2d5917e6c44f9e4c283b55376f1
patrickfeltes <pj6444@gmail.com>
2019-09-27 15:12:22 -0500

    Change submit endpoint to receive json object

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO
+import json
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
@@ -10,7 +11,10 @@ def root():
 
 @socketio.on('submit')
 def handle_submit(ans):
-  print(f'Submitted {ans}')
+  parsed_ans = json.loads(ans)
+  email = parsed_ans['email']
+  letter = parsed_ans['letter']
+  print(f'{email} submitted {letter}')
 
 if __name__ == '__main__':
   socketio.run(app, debug=True)
