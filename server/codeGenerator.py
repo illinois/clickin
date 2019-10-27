a56395a2226b19deacaa9de1bfe2631b8c336d3e
Marcin <mpitera483@gmail.com>
2019-10-27 14:08:15 -0500

    moved bad words list to seperate txt file

--- server/codeGenerator.py
+++ server/codeGenerator.py
@@ -1,9 +1,6 @@
 import random
 import string
 
-
-BAD_WORDS = ["ass","fuc","fuk","fuq","fux","fck","coc","cok","coq","kox","koc","kok","koq","cac","cak","caq","kac","kak","kaq","dic","dik","diq","dix","dck","pns","psy","fag","fgt","ngr","nig","cnt","knt","sht","dsh","twt","bch","cum","clt","kum","klt","suc","suk","suq","sck","lic","lik","liq","lck","jiz","jzz","gay","gey","gei","gai","vag","vgn","sjv","fap","prn","lol","jew","joo","gvr","pus","pis","pss","snm","tit","fku","fcu","fqu","hor","slt","jap","wop","kkk","kik","kyk","kyc","kyq","dyk","dyq","dyc","kkk","jyz","prk","prc","prq","mic","mik","miq","myc","myk","myq","guc","guk","guq","giz","gzz","sex","sxx","sxi","sxe","sxy","xxx","wac","wak","waq","wck","pot","thc","vaj","vjn","nut","std","lsd","poo","azn","pcp","dmn","orl","anl","ans","muf","mff","phk","phc","phq","xtc","tok","toc","toq","mlf","rac","rak","raq","rck","sac","sak","saq","pms","nad","ndz","nds","wtf","sol","sob","fob","sfu"]
-
 def generateCode(numOfChars):
     urlBase = "click.in/"
     code = ""
@@ -24,4 +21,6 @@ def generateRandomElems(numOfChars):
     return code
 
 def isValid(code):
-    return code or not(BAD_WORDS in code)
+    bad_words_file = open('bad_words.txt', 'r')
+    bad_words_list = bad_words_file.read().split(' ')
+    return code or not bad_words_list in code
