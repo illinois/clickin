a4067682c6611832e6acc9df47468ee1a6b5f854
Marcin <mpitera483@gmail.com>
2019-10-28 13:17:44 -0500

    remove '' from bad_words.txt and fix validity

--- server/code_generator.py
+++ server/code_generator.py
@@ -1,14 +1,14 @@
 import random
 import string
 
-bad_words_file = open('clickin/server/bad_words.txt', 'r')
-BAD_WORDS_LIST = bad_words_file.read().split(' ')
+bad_words_file = open('server/bad_words.txt', 'r')
+BAD_WORDS_LIST = bad_words_file.read().split('\n')
 bad_words_file.close()
 
 def generate_code(num_of_chars):
     code = ''
 
-    while not is_valid(code.lower()):
+    while not is_valid(code):
         code = generate_random_elems(num_of_chars)
 
     return code
@@ -24,11 +24,11 @@ def generate_random_elems(num_of_chars):
     return code
 
 def is_valid(code):
-    if not code:
+    if not "".join(code.split()):
         return False
 
     for bad_word in BAD_WORDS_LIST:
-        if bad_word in code:
+        if bad_word in code.lower():
             return False
 
     return True
