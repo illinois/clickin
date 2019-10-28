bd382907f66607184be2a8f4c9fca16a320f79ba
Marcin <mpitera483@gmail.com>
2019-10-28 13:02:24 -0500

    wrote tests for is_valid

---
+++ server/code_generator_test.py
@@ -0,0 +1,15 @@
+from code_generator import generate_code, generate_random_elems, is_valid
+from random import randint
+import pytest
+
+@pytest.mark.parametrize(('code_input'), [(''), (' '), ('  ')])
+def test_is_valid_returns_false_for_empty_input(code_input):
+    assert is_valid(code_input) == False
+
+@pytest.mark.parametrize(('code_input'), [('aSS'), ('ass'), ('12ass'), ('12Ass'), ('ass12'), ('Ass21')])
+def test_is_valid_returns_false_for_invalid_input(code_input):
+    assert is_valid(code_input) == False
+
+@pytest.mark.parametrize(('code_input'), [('1'), ('541'), ('a'), ('abc'), ('A'), ('ABC'), ('10as13'), ('10asAFD13'), ('ASDas'), ('10AS13')])
+def test_is_valid_returns_true_for_valid_input(code_input):
+    assert is_valid(code_input) == True
