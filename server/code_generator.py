58de0aaae114803b5e9889fb35ed9c73e87ebaf9
Marcin <mpitera483@gmail.com>
2019-11-05 01:56:51 -0600

    removed docstrings

--- server/code_generator.py
+++ server/code_generator.py
@@ -6,18 +6,6 @@ BAD_WORDS_LIST = bad_words_file.read().split('\n')
 bad_words_file.close()
 
 def generate_code(num_of_chars):
-    """ 
-    Generates a valid code
-
-    Re-generates random elements until code is valid
-  
-    Parameters: 
-    num_of_chars (int): the intended length of code
-  
-    Returns: 
-    String: A valid code of length num_of_chars
-  
-    """
     code = ''
 
     while not is_valid(code):
@@ -26,18 +14,6 @@ def generate_code(num_of_chars):
     return code
 
 def generate_random_elems(num_of_chars):
-    """ 
-    Generates a string of random elements
-
-    The random elements are pulled from a pool of all letters both upper and lower and all digits
-  
-    Parameters: 
-    num_of_chars (int): The amount of chars to be generated
-  
-    Returns: 
-    String: A string of random elements of length num_of_chars
-  
-    """
     POSSIBLE_CHARS = string.ascii_letters + string.digits
     code = ''
 
@@ -48,18 +24,6 @@ def generate_random_elems(num_of_chars):
     return code
 
 def is_valid(code):
-    """ 
-    Checks validity of given code
-
-    Validity checks that code is not an empty string and that it doesn't contain any illegal words
-  
-    Parameters: 
-    code (string): The string to be verified
-  
-    Returns: 
-    boolean: Whether the code is valid
-  
-    """
     if not "".join(code.split()):
         return False
 
