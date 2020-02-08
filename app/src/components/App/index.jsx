9dd579210f6b1113aa140c1da1924c0e0c181ba4
tdog191 <twarther17@gmail.com>
2020-02-07 23:25:52 -0600

    Start Course Session React Component
    Instructors will need a webpage(s) to display the questions associated
    with a course session. Further discussion is needed on the styling and
    layout of these pages, so this commit is primarily intended as a proof
    of concept.
    
    The UI's styling is rendered by react-bootstrap. react-bootsrap is a
    complete re-implementation of Boostrap components in React. Like
    Bootstrap, it defines many CSS classes to make pretty UI elements and
    uses flexbox to make flexibly layouts. More info can be found at
    https://bre.is/JX2hfmU4.
    
    Issues: #32

--- app/src/components/App/index.jsx
+++ app/src/components/App/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Remote from 'components/Remote';
+import Session from 'components/Instructor/Course/Session';
 
 export default class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Route exact path="/" component={Remote} />
+        <Route exact path="/instructors/wade/cs225/sessions/" component={Session} />
       </BrowserRouter>
     );
   }
