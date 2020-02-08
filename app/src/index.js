ebb182846411f5173610d247e48842895ec17e43
tdog191 <twarther17@gmail.com>
2020-02-07 21:02:47 -0600

    Revert "Start Student React component"
    This reverts commit 042edf75239048ccb793a095117bb18fef647c20.

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
